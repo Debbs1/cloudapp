f3e4dc9d2529f616e9658b5f6674c33611dd0c77
Debbs1 <62051244+Debbs1@users.noreply.github.com>
2020-09-17 12:21:24 +0100

    commit changes

---
+++ public/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  //so to purge just uncomment the below line and comment line number 5 then in the console run the 'npm run custom'
+  //  purge: { enabled: true, content: ['build/*.html', 'build/*.js'] },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        RockSalt: ['Rock Salt'],
+        Risque: ['Risque'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
